5134225d160fafb8d5a3aa39d8b15a262f85b249
haytham.elzamek <haytham.elzamek@v-apps.co>
2017-08-21 14:26:16 +0200

    initial

--- conf.js
+++ conf.js
@@ -1,6 +1,17 @@
 exports.config = {
+
+    allScriptsTimeout: 11000,
+
+    baseUrl: 'http://localhost:8000/',
+
     capabilities: {
-        'browserName': 'firefox'
+        'browserName': 'chrome'
+    },
+
+    framework: 'jasmine',
+
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
     } ,
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['todo-spec.js']
